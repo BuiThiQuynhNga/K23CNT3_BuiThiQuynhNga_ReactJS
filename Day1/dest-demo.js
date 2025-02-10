8065c0c7c6b396a9863ebada6988c8a54e828715
unknown <phamlucvx1234@gmail.com>
2025-02-10 20:26:04 +0700

    K23CNT3_BTQN_ReactJS

---
+++ Day1/dest-demo.js
@@ -0,0 +1,10 @@
+//
+var array = [11,22,33,44];
+var [x,y] = array;
+console.log("x=",x);
+console.log("y=",y);
+
+var[a,b,c] = array;
+console.log("a=",a);
+
+
---
+++ Day1/var-demo.js
@@ -0,0 +1,20 @@
+var x=100; //khai báo global
+function varDemo(){
+    console.log("x ben trong ham:",x);
+}
+//
+    console.log("x ben ngoai ham:",x);
+// thực thi hàm:
+varDemo();
+//local
+function fn_demo(){
+    var y=200; //local
+    console.log("y bên trong hàm:",y);
+}
+fn_demo()
+//y bên ngoài hàm
+console.log("y bên ngoài hàm:", y); //==error
+ 
+
+//ví dụ
+
