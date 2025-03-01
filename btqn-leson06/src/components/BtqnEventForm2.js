728a5202ff0bf964e0cbaf2d75f0ddd32ec49937
unknown <phamlucvx1234@gmail.com>
2025-03-01 18:11:05 +0700

    btqn_leson06

---
+++ btqn-leson06/src/components/BtqnEventForm2.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+ class BtqnEvenForm2 extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            btqnStudentName:"Quỳnh Nga"
+        }
+    }
+export default
